e77bac59279c2d08547dad8316acc72f28a66800
Tinka8 <lenmatka818@gmail.com>
2021-02-09 19:42:55 +0100

    doplnuje zoznam hrdinov

--- tinkabot.py
+++ tinkabot.py
@@ -11,22 +11,133 @@ bot = commands.Bot(
 )
 
 
-@bot.command(name='test')
+@bot.command(name='test')                                             
 async def test(ctx):
     await ctx.send('Funguje to')
 
-@bot.command(name='tina')
+@bot.command(name='tina')                                              
 async def tina(ctx):
-    await ctx.send('jsem tina, je mi 17 a uz 4 roky piju kavu')
+    await ctx.send('jsem tina, je mi 17 a uz 4 roky piju kavu')       
 
-@bot.command(name='pick')
+@bot.command(name='pick')                                             
 async def pick(ctx):
-    heroes = [
+    heroes = [                                                     
+        "Abaddon",                                                     # strenght 
+        "Alchemist",	
+        "Axe",	
+        "Beastmaster",	
+        "Brewmaster",
+        "Bristleback",
+        "Centaur Warrunner",
+        "Chaos Knight",
+        "Clockwerk",
+        "Doom",
+        "Dragon Knight",
+        "Earth Spirit",
+        "Earthshaker",
+        "Elder Titan",
+        "Huskar",
+        "Io",
+        "Kunkka",
+        "Legion Commander",
+        "Lifestealer",
+        "Lycan",
+        "Magnus",
+        "Night Stalker",
+        "Omniknight",
+        "Phoenix",	
+        "Pudge",
+        "Sand King",
+        "Slardar",
+        "Spirit Breaker",
+        "Sven",
+        "Tidehunter",
+        "Timbersaw",
+        "Tiny",
+        "Treant Protector",
+        "Tusk",
+        "Underlord",
+        "Undying",
+        "Wraith King",
+        "Anti-Mage",                                                   # agility
+        "Arc Warden",
+        "Bloodseeker",
+        "Bounty Hunter",
+        "Broodmother",
+        "Clinkz",
+        "Drow Ranger",
+        "Ember Spirit",
+        "Faceless Void",
+        "Gyrocopter",
+        "Juggernaut",
+        "Lone Druid",
+        "Luna",
+        "Medusa",
+        "Meepo",
+        "Mirana",
+        "Monkey King",
+        "Morphling",
+        "Naga Siren",
+        "Nyx Assassin",
+        "Pangolier",
+        "Phantom Assassin",
+        "Phantom Lancer",
+        "Razor",
+        "Riki",
+        "Shadow Fiend",
         "Slark",
-        "Oracle",
+        "Sniper",
+        "Spectre",
+        "Templar Assassin",
+        "Terrorblade",
+        "Troll Warlord",
+        "Ursa",
+        "Vengeful Spirit",
+        "Venomancer",
+        "Viper",
+        "Weaver",
+        "Ancient Apparition",                                           # intelligence
         "Bane",
-        "Axe"
-    ]
+        "Batrider",
+        "Chen",
+        "Crystal Maiden",
+        "Dark Seer",
+        "Dark Willow",
+        "Dazzle",
+        "Death Prophet",
+        "Disruptor",
+        "Enchantress",
+        "Enigma",
+        "Invoker",
+        "Jakiro",
+        "Keeper of the Light", 
+        "Leshrac",
+        "Lich",
+        "Lina",
+        "Lion",
+        "Nature’s Prophet",
+        "Necrophos",
+        "Ogre Magi",
+        "Oracle",
+        "Outworld Devourer",
+    	"Puck",
+        "Pugna",
+        "Queen of Pain",
+        "Rubick",
+        "Shadow Demon",
+        "Shadow Shaman",
+        "Silencer",
+        "Skywrath Mage",
+        "Storm Spirit",
+        "Techies",
+        "Tinker",
+        "Visage",
+        "Warlock",
+        "Windranger",
+        "Winter Wyvern",
+        "Witch Doctor",
+        "Zeus",                                                                      
+        ]  
     await ctx.send(random.choice(heroes))
 
 
