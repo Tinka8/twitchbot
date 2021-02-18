da1f84b00db8af45f1fd3ef94fe7e80d501c63c4
Tinka8 <lenmatka818@gmail.com>
2021-02-18 22:10:48 +0100

    resources

--- tinkabot.py
+++ tinkabot.py
@@ -1,6 +1,12 @@
 import os
 import random
 from twitchio.ext import commands
+from resources.heroes import heroes
+from resources.heroes import pos1
+from resources.heroes import pos2
+from resources.heroes import pos3
+from resources.heroes import pos4
+from resources.heroes import pos5
 
 bot = commands.Bot(
     irc_token=os.environ['TMI_TOKEN'],
@@ -27,166 +33,9 @@ async def tina(ctx):
     await ctx.send('jsem tina, je mi 17 a uz 4 roky piju kavu')       
 
 
-
-# List of heroes
-heroes = [                                                     
-    "Abaddon",                                                     # strenght 
-    "Alchemist",	
-    "Axe",
-    "Beastmaster",	
-    "Brewmaster",
-    "Bristleback",
-    "Centaur Warrunner",
-    "Chaos Knight",
-    "Clockwerk",
-    "Doom",
-    "Dragon Knight",
-    "Earth Spirit",
-    "Earthshaker",
-    "Elder Titan",
-    "Huskar",
-    "Io",
-    "Kunkka",
-    "Legion Commander",
-    "Lifestealer",
-    "Lycan",
-    "Magnus",
-    "Night Stalker",
-    "Omniknight",
-    "Phoenix",	
-    "Pudge",
-    "Sand King",
-    "Slardar",
-    "Spirit Breaker",
-    "Sven",
-    "Tidehunter",
-    "Timbersaw",
-    "Tiny",
-    "Treant Protector",
-    "Tusk",
-    "Underlord",
-    "Undying",
-    "Wraith King",
-    "Anti-Mage",                                                   # agility
-    "Arc Warden",
-    "Bloodseeker",
-    "Bounty Hunter",
-    "Broodmother",
-    "Clinkz",
-    "Drow Ranger",
-    "Ember Spirit",
-    "Faceless Void",
-    "Gyrocopter",
-    "Juggernaut",
-    "Lone Druid",
-    "Luna",
-    "Medusa",
-    "Meepo",
-    "Mirana",
-    "Monkey King",
-    "Morphling",
-    "Naga Siren",
-    "Nyx Assassin",
-    "Pangolier",
-    "Phantom Assassin",
-    "Phantom Lancer",
-    "Razor",
-    "Riki",
-    "Shadow Fiend",
-    "Slark",
-    "Sniper",
-    "Spectre",
-    "Templar Assassin",
-    "Terrorblade",
-    "Troll Warlord",
-    "Ursa",
-    "Vengeful Spirit",
-    "Venomancer",
-    "Viper",
-    "Weaver",
-    "Ancient Apparition",                                           # intelligence
-    "Bane",
-    "Batrider",
-    "Chen",
-    "Crystal Maiden",
-    "Dark Seer",
-    "Dark Willow",
-    "Dazzle",
-    "Death Prophet",
-    "Disruptor",
-    "Enchantress",
-    "Enigma",
-    "Invoker",
-    "Jakiro",
-    "Keeper of the Light", 
-    "Leshrac",
-    "Lich",
-    "Lina",
-    "Lion",
-    "Nature’s Prophet",
-    "Necrophos",
-    "Ogre Magi",
-    "Oracle",
-    "Outworld Devourer",
-    "Puck",
-    "Pugna",
-    "Queen of Pain",
-    "Rubick",
-    "Shadow Demon",
-    "Shadow Shaman",
-    "Silencer",
-    "Skywrath Mage",
-    "Storm Spirit",
-    "Techies",
-    "Tinker",
-    "Visage",
-    "Warlock",
-    "Windranger",
-    "Winter Wyvern",
-    "Witch Doctor",
-    "Zeus",                                                                      
-]  
-
-
 picked = [
 
 ]
-
-
-pos1 = [
-    "Slark",
-    "Spectre",
-    "Wraith King",
-]
-
-
-pos2 = [
-    "Storm Spirit",
-    "Lina",
-    "Puck",
-]
-
-
-pos3 = [
-    "Axe",
-    "Bristleback",
-    "Beastmaster",
-]
-
-
-pos4 = [
-    "Mirana",
-    "Jakiro",
-    "Dark Willow",
-]
-
-
-pos5 = [
-    "Oracle",
-    "Witch Doctor",
-    "Dazzle"
-]
-
 
 
 def selectHero(message = "!pick"):
@@ -230,7 +79,5 @@ async def repick(ctx):
 
 if __name__ == "__main__":
     bot.run()
-
-
 
 
