1db22c0594440b8d6882b96d33154f75975e10f0
Tinka8 <lenmatka818@gmail.com>
2021-03-01 15:40:15 +0100

    descripton of commands

---
+++ resources/commands.py
@@ -0,0 +1,9 @@
+fullList = {
+  '!pick': 'Vyberie nahodneho hrdinu zo zoznamu',
+  '!repick': 'Vyberie nahodneho hrdinu zo zoznamu, ktory este nebol picknuty',
+  '!tina': 'Predstavi tinu',
+  '!test': 'Otestuje, ze bot funguje',
+  '!wl': 'Pocet vyhier a prehier od zaciatku streamu',
+  '!kda': 'KDA priemer od zaciatku streamu',
+  '!chant': 'Random blbost do chatu'
+}
