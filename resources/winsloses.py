0d77cb98040f74943633759d5e4a372bf2fe0aed
Tinka8 <lenmatka818@gmail.com>
2021-03-04 15:13:12 +0100

    edit

--- resources/winsloses.py
+++ resources/winsloses.py
@@ -8,7 +8,7 @@ def getRecentMatches(playerId):
     return response
 
 def getWinsAndLoses(playerId, since):
-
+    
     output = {
         'wins': 0,
         'loses': 0
@@ -19,22 +19,22 @@ def getWinsAndLoses(playerId, since):
     
     for match in json:
     
-        if (match["start_time"] > since):
+        if (match['start_time'] > since):
             
             # fatallik was radiant and won 
-            if match["radiant_win"] == True and match["player_slot"] <= 127:          
+            if match['radiant_win'] == True and match['player_slot'] <= 127:          
                 output['wins'] += 1
 
             # fatallik was radiant and lost
-            elif match["radiant_win"] == True and match["player_slot"] >= 127:        
+            elif match['radiant_win'] == True and match['player_slot'] >= 127:        
                 output['loses'] += 1
 
             # fatallik was dire and won
-            elif match["radiant_win"] == False and match["player_slot"] >= 128:      
+            elif match['radiant_win'] == False and match['player_slot'] >= 128:      
                 output['wins'] += 1
 
             # fatallik was dire and lost
-            elif match["radiant_win"] == False and match["player_slot"] <= 128:       
+            elif match['radiant_win'] == False and match['player_slot'] <= 128:       
                 output['loses'] += 1 
 
     print(output['wins'])
