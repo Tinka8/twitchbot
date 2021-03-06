74962be0dc11ef3361cee69a2b3b42ae81caafc4
Tinka8 <lenmatka818@gmail.com>
2021-03-06 18:20:43 +0100

    edit

--- tinkabot.py
+++ tinkabot.py
@@ -75,17 +75,6 @@ async def repick(ctx):
         return False 
 
     await replaceSelected(ctx, selected)
-
-# ! chant command
-@bot.command(name='chant')
-async def chant(ctx):
-    for i in range(0,6,+1):
-        await ctx.send((i-1)*'* ')
-
-    await ctx.send('* * * * * fatal')
-
-    for j in reversed(range(0,6,+1)):
-        await ctx.send((j-1)*'* ')
     
 # ! wl command
 @bot.command(name='wl')
